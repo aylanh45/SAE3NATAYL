a68cc2c3bc9194412b1790beff0b0fef4b9b0cf7
aylanh45 <aylan.haddouchi@edu.univ-fcomte.fr>
2024-11-30 14:48:45 +0100

    wip

--- app.py
+++ app.py
@@ -3,6 +3,8 @@
 from flask import Flask, request, render_template, redirect, flash
 from more_itertools.recipes import grouper
 #mysql --user=nfoucaul  --password=mdp --host=serveurmysql --database=BDD_nfoucaul
+
+#mysql --user=ahaddou2  --password=mdp --host=serveurmysql --database=BDD_ahaddou2
 app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 app.secret_key = 'une cle(token) : grain de sel(any random string)'
@@ -17,7 +19,7 @@ def get_db():
             host="serveurmysql",                 # à modifier
             user="nfoucaul",                     # à modifier
             password="mdp",                # à modifier
-            database="BDD_nfoucaul",        # à modifier
+            database="BDD_nfoucaul_tp",        # à modifier
             charset='utf8mb4',
             cursorclass=pymysql.cursors.DictCursor
         )
@@ -29,14 +31,15 @@ def teardown_db(exception):
     if db is not None:
         db.close()
 @app.route('/')
+def show_accueil():
+    return render_template('layout.html')
 @app.route('/mission/show')
 def show_missions():
     mycursor = get_db().cursor()
-    sql= ''' SELECT Id_Mission AS id, DateDébut AS DateDébut, DateFin AS DateFin, Kg_CO2 AS Kg_CO2 
-    FROM Mission
-    ORDER BY DateDébut DESC; '''
+    sql= ''' SELECT Mission.Id_Mission AS id, Mission.DateDébut AS DateDébut, Mission.DateFin AS DateFin, Mission.Kg_CO2 AS Kg_CO2, Agent.Nom AS Nom, Mission.Id_Agent
+        FROM Mission
+        INNER JOIN Agent ON Mission.Id_Agent = Agent.Id_Agent'''
     mycursor.execute(sql)
-
     liste_missions = mycursor.fetchall()
     return render_template('mission/show_mission.html', missions=liste_missions )
 
@@ -44,7 +47,15 @@ def show_missions():
 @app.route('/mission/add', methods=['GET'])
 def add_mission():
     print('''affichage du formulaire pour saisir une mission''')
-    return render_template('mission/add_mission.html')
+    mycursor = get_db().cursor()
+    sql = '''SELECT Id_TypeMission, LibelleTypeMission
+    FROM Type_mission'''
+    mycursor.execute(sql)
+    type_missions = mycursor.fetchall()
+    sql_agents ='''SELECT Id_Agent, Nom FROM Agent'''
+    mycursor.execute(sql_agents)
+    agents = mycursor.fetchall()
+    return render_template('mission/add_mission.html', type_missions=type_missions, agents=agents)
 
 @app.route('/mission/delete')
 def delete_mission():
@@ -57,64 +68,153 @@ def delete_mission():
     sql = ''' DELETE FROM Mission WHERE Id_Mission = %s '''
     tuple_param=(id)
     mycursor.execute(sql, tuple_param)
-
     get_db().commit()
     return redirect('/mission/show')
 
 @app.route('/mission/edit', methods=['GET'])
-def edit_etudiant():
-    print('''affichage du formulaire pour modifier une mission''')
-    print(request.args.get('id'))
-    id=request.args.get('id')
-    if id != None and id.isnumeric():
-        indice = int(id)
-        mycursor = get_db().cursor()
-        sql = ''' SELECT Id_Mission AS id, DateDébut AS DateDébut, DateFin AS DateFin, Kg_CO2 AS Kg_CO2
+def edit_mission():
+    mycursor = get_db().cursor()
+    id=request.args.get('id','')
+    sql = ''' SELECT Mission.Id_Mission AS id, Mission.DateDébut AS DateDébut, Mission.DateFin AS DateFin, Mission.Kg_CO2 AS Kg_CO2, Agent.Nom AS Nom, Mission.Id_TypeMission AS Id_TypeMission, Mission.Id_Agent
             FROM Mission
-            WHERE Id_Mission=%s;'''
-        mycursor.execute(sql, (id))
-        mission = mycursor.fetchone()
-    else:
-        mission=[]
-    return render_template('mission/edit_mission.html', mission=mission)
+            INNER JOIN Agent ON Mission.Id_Agent = Agent.Id_Agent
+            WHERE Mission.Id_Mission = %s;'''
+    mycursor.execute(sql, (id,))
+    mission = mycursor.fetchone()
+    sql_agents='''SELECT Id_Agent, Nom FROM Agent'''
+    mycursor.execute(sql_agents)
+    agents = mycursor.fetchall()
+    sql_types_mission = '''SELECT Id_TypeMission, LibelleTypeMission FROM Type_mission'''
+    mycursor.execute(sql_types_mission)
+    type_missions = mycursor.fetchall()
+    return render_template('mission/edit_mission.html', mission=mission, agents=agents, type_missions=type_missions)
 
 
 @app.route('/mission/add', methods=['POST'])
 def valid_add_mission():
     print('''ajout de la mission dans le tableau''')
-    DateDébut = request.form.get('DateDébut')
-    DateFin = request.form.get('DateFin')
-    Kg_CO2 = request.form.get('Kg_CO2')
-    message = 'date de début :' + DateDébut + ' - Date de fin :' + DateFin  + ' - Killogramme de CO2 : ' + Kg_CO2
+    DateDébut = request.form.get('DateDébut') or ''
+    DateFin = request.form.get('DateFin') or ''
+    Kg_CO2 = request.form.get('Kg_CO2') or ''
+    Id_TypeMission = request.form.get('Id_TypeMission') or ''
+    Id_Agent = request.form.get('Id_Agent')
+    message = 'date de début :' + DateDébut + ' - Date de fin :' + DateFin  + ' - Killogramme de CO2 : ' + Kg_CO2 + '- le type de mission a pour identifiant' + Id_TypeMission + '- une mission réalisée par l agent d identifiant' + Id_Agent
     print(message)
     #insert
     mycursor = get_db().cursor()
-    sql = ''' INSERT INTO Mission (Id_Mission, DateDébut, DateFin, Kg_CO2) 
-    VALUES (NULL, %s, %s); '''
-    tuple_param = (DateDébut, DateFin, Kg_CO2)
+    sql = ''' INSERT INTO Mission (DateDébut, DateFin, Kg_CO2,Id_TypeMission, Id_Agent) 
+    VALUES (%s, %s, %s, %s, %s); '''
+    tuple_param = (DateDébut, DateFin, Kg_CO2, Id_TypeMission, Id_Agent)
     mycursor.execute(sql, tuple_param)
-
-
     get_db().commit()
     return redirect('/mission/show')
+
 
 @app.route('/mission/edit', methods=['POST'])
-def valid_edit_etudiant():
-    print('''modification de la mission dans le tableau''')
-    id = request.form.get('id')
-    DateDébut = request.form.get('DateDébut')
-    DateFin = request.form.get('DateFin')
-    Kg_CO2 = request.form.get('Kg_CO2')
-    message = 'Date de début :' + DateDébut + ' - Date de fin :' + DateFin + ' - Kilogrammes de CO2 ' + Kg_CO2 + ' pour l etudiant d identifiant :' + id
-    print(message)
-    #update
+def valid_edit_mission():
+    print('Modification de la mission dans le tableau')
+
+    Id_Mission = request.form.get('Id_Mission')
+    DateDébut = request.form.get('DateDébut') or None
+    DateFin = request.form.get('DateFin') or None
+    Kg_CO2 = request.form.get('Kg_CO2') or None
+    Id_TypeMission = request.form.get('Id_TypeMission') or None
+    Id_Agent = request.form.get('Id_Agent') or None
+    sql = ''' UPDATE Mission 
+              SET DateDébut = %s, DateFin = %s, Kg_CO2 = %s, Id_Agent = %s, Id_TypeMission = %s
+              WHERE Id_Mission = %s; '''
+    tuple_param = (DateDébut, DateFin, Kg_CO2, Id_Agent, Id_TypeMission, Id_Mission)
     mycursor = get_db().cursor()
-    sql = ''' UPDATE Mission SET DateDébut = %s, DateDébut = %s, Kg_CO2 = %s ,WHERE Id_Mission = %s;'''
-    tuple_param = (DateDébut, DateFin, Kg_CO2 ,id)
     mycursor.execute(sql, tuple_param)
     get_db().commit()
+
     return redirect('/mission/show')
+
+
+
+
+@app.route('/etape/show')
+def show_etapes():
+    mycursor = get_db().cursor()
+    sql= ''' SELECT Etape.Id_Etape AS id, Etape.DistanceParcourue AS DistanceParcourue, Etape.Id_MoyenTransport AS Id_MoyenTransport, Etape.Id_Lieu_depart AS Id_Lieu_depart, Etape.Id_Lieu_arrivee AS Id_Lieu_arrivee, Etape.Id_Mission AS Id_Mission 
+    FROM Etape'''
+    mycursor.execute(sql)
+    liste_etapes = mycursor.fetchall()
+    return render_template('etape/show_etape.html', etapes=liste_etapes )
+
+@app.route('/etudiant/add', methods=['GET'])
+def add_etudiant():
+    print('''affichage du formulaire pour saisir un étudiant''')
+    return render_template('etape/add_etape.html')
+@app.route('/etape/delete')
+def delete_etudiant():
+    print('''suppression d'une étape''')
+    print(request.args)
+    print(request.args.get('id'))
+    id=request.args.get('id')
+#delete
+    mycursor = get_db().cursor()
+    sql = ''' DELETE FROM etape WHERE Id_Etape = %s '''
+    tuple_param=(id)
+    mycursor.execute(sql, tuple_param)
+    get_db().commit()
+    return redirect('/etape/show')
+
+@app.route('/etape/edit', methods=['GET'])
+def edit_etape():
+    print('''affichage du formulaire pour modifier une étape''')
+    print(request.args.get('id'))
+    id=request.args.get('id')
+    if id != None and id.isnumeric():
+        indice = int(id)
+        mycursor = get_db().cursor()
+        sql = ''' SELECT Etape.Id_Etape AS id, Etape.DistanceParcourue AS DistanceParcourue, Etape.Id_MoyenTransport AS Id_MoyenTransport, Etape.Id_Lieu_depart AS lieu_depart, Etape.Id_Lieu_arrivee AS Id_Lieu_arrivee, Etape.Id_Mission AS Id_Mission
+            FROM Etape
+            WHERE Etape.Id_Etape = %s;'''
+        mycursor.execute(sql, (id))
+        etape = mycursor.fetchone()
+    return render_template('etape/edit_etape.html', etape=etape)
+
+
+@app.route('/etape/add', methods=['POST'])
+def valid_add_etape():
+    print('''ajout de l'étape dans le tableau''')
+    Id_Etape = request.form.get('Id_Etape')
+    DistanceParcourue = request.form.get('DistanceParcourue') or ''
+    Id_MoyenTransport = request.form.get('Id_MoyenTransport') or ''
+    Id_Lieu_depart = request.form.get('Id_Lieu_depart') or ''
+    Id_Lieu_arrivee = request.form.get('Id_Lieu_arrivee') or ''
+    Id_Mission = request.form.get('Id_Mission') or ''
+    message = 'distance parcourue :' + DistanceParcourue + ' - le moyen de transport :' + Id_MoyenTransport  + ' - le lieu de départ : ' + Id_Lieu_depart + '- le lieu d arrivee : ' + Id_Lieu_arrivee + '- la mission :' + Id_Mission + '- l etape a pour identifiant : ' + Id_Etape
+    print(message)
+    #insert
+    mycursor = get_db().cursor()
+    sql = ''' INSERT INTO Etape (DistanceParcourue, Id_MoyenTransport, Id_Lieu_arrivee,Id_Lieu_depart, Id_Mission) 
+        VALUES (%s, %s, %s, %s, %s); '''
+    tuple_param = (DistanceParcourue, Id_MoyenTransport, Id_Lieu_arrivee, Id_Lieu_depart, Id_Mission)
+    mycursor.execute(sql, tuple_param)
+    get_db().commit()
+    return redirect('/etape/show')
+
+@app.route('/etape/edit', methods=['POST'])
+def valid_edit_etape():
+    print('''modification de l'étapedans le tableau''')
+    IdEtape = request.form.get('IdEtape')
+    DistanceParcourue = request.form.get('DistanceParcourue') or None
+    Id_MoyenTransport = request.form.get('Id_MoyenTransport') or None
+    Id_Lieu_arrivee = request.form.get('Id_lieu_arrivee') or None
+    Id_Lieu_depart = request.form.get('Id_Lieu_depart') or None
+    Id_Mission = request.form.get('Id_Mission') or None
+    sql = ''' UPDATE Etape
+                SET Distanceparcourue = %s, Id_MoyenTransport = %s, Id_Lieu_arrivee = %s, Id_Lieu_depart = %s, Id_Mission = %s
+                WHERE Id_Etape = %s; '''
+    tuple_param = (DistanceParcourue, Id_MoyenTransport, Id_Lieu_arrivee, Id_Lieu_depart, Id_Mission, Id_Etape)
+    mycursor = get_db().cursor()
+    mycursor.execute(sql, tuple_param)
+    get_db().commit()
+
+    return redirect('/mission/show')
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
-
--- launcher.sh
+++ launcher.sh
@@ -1,1 +1,4 @@
-flask --debug  --app app  run   --host 0.0.0.0
+flask --debug  --app app  run  --host 0.0.0.0
+
+#aylanh45
+#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
